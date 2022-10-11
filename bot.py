a58fb7fb8e798029c8fd03960e271d9e4287d91f
jrodr117 <jrodr113@msudenver.edu>
2022-10-11 17:12:41 -0600

    driver variable holds an instance of chrome for us and .get navigates to our page.

--- bot.py
+++ bot.py
@@ -2,7 +2,8 @@ from selenium import webdriver
 from configkeys import keys
 
 def order(key):
-    
+    driver = webdriver.Chrome('chromedriver')
+    driver.get(key['product_url'])
 
 if __name__ == '__main__':
     order(keys)
