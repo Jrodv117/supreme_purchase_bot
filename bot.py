df84d1a4af71c301b35fe93aaab6d3f33e0a7dae
jrodr117 <jrodr113@msudenver.edu>
2022-10-11 17:44:03 -0600

    trying to get our script to click the add to cart button

--- bot.py
+++ bot.py
@@ -1,11 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
 from configkeys import keys
 
 def order(key):
     s = Service('./chromedriver')
     driver = webdriver.Chrome(service=s)
     driver.get(key['product_url'])
-
+    driver.find_elements(By.XPATH, '//*[@id="add-remove-buttons"]/input').click()
+    
 if __name__ == '__main__':
     order(keys)
