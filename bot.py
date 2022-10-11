78d2f0b855d8762e46e362c2c62c8d8df37be3af
jrodr117 <jrodr113@msudenver.edu>
2022-10-11 17:10:17 -0600

    added order function and added product_url key:value in our keys dictionary

--- bot.py
+++ bot.py
@@ -1,4 +1,8 @@
 from selenium import webdriver
+from configkeys import keys
+
+def order(key):
+    
 
 if __name__ == '__main__':
-    order()
+    order(keys)
---
+++ configkeys.py
@@ -0,0 +1,4 @@
+keys = {
+    'product_url': 'https://www.supremenewyork.com/shop/tops-sweaters/dbkytp5xm/y2pcme8yl'
+    'name' : 'Joe',
+}
