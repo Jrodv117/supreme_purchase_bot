873220ed11cda5e170b6982a1e7d19a7203f6a5d
jrodr117 <jrodr113@msudenver.edu>
2022-10-11 18:04:14 -0600

    got first button click to work

--- bot.py
+++ bot.py
@@ -1,13 +1,15 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
+# from selenium.webdriver.common.by import By
+# from selenium.webdriver.common.action_chains import ActionChains
 from configkeys import keys
+import time
 
 def order(key):
     s = Service('./chromedriver')
     driver = webdriver.Chrome(service=s)
     driver.get(key['product_url'])
-    driver.find_elements(By.XPATH, '//*[@id="add-remove-buttons"]/input').click()
-    
+    driver.find_element('xpath', '/html/body/div/div/div[2]/div/form/fieldset[2]/input').click()
+    driver.find_element('xpath', '').click()
 if __name__ == '__main__':
     order(keys)
