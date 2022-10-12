26d55f648cf1fc0772072d9725c076efeebaeca5
jrodr117 <jrodr113@msudenver.edu>
2022-10-11 18:21:41 -0600

    added the click chechout path and it works

--- bot.py
+++ bot.py
@@ -10,6 +10,6 @@ def order(key):
     driver = webdriver.Chrome(service=s)
     driver.get(key['product_url'])
     driver.find_element('xpath', '/html/body/div/div/div[2]/div/form/fieldset[2]/input').click()
-    driver.find_element('xpath', '').click()
+    driver.find_element('xpath', '//*[@id="cart"]/a[2]').click()
 if __name__ == '__main__':
     order(keys)
